01c93d1718446b6159b47d61cd7dbd39ad289997
cgragg6155 <49204163+cgragg6155@users.noreply.github.com>
2019-04-16 17:00:38 -0400

    Update SpecificFlightListController.java
    added imports for fxml bs

--- src/application/SpecificFlightListController.java
+++ src/application/SpecificFlightListController.java
@@ -1,7 +1,29 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class SpecificFlightListController extends MainController
 {
+	    @FXML
+	    private TextField inputFlightNumber;
+
+	    @FXML
+	    private Button displayFlightSeatsButton;
+
+	    @FXML
+	    private TextArea specificFlightSeats;
+	    
+	    public String flightSeatFile=FlightSeating.getSeatingChart(inputFlightNumber.getText());
+
+	    @FXML
+	    void displayFlightSeats(ActionEvent event) 
+	    {
+	    	
+	    }
 	public void goToMainMenu()
 	{
 		changeScene("mainMenu");
