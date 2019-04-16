223ada9af416720e9bc15d3ee611fa6185d7d7a8
Luke Mirman <OrangeJuicePanda1@gmail.com>
2019-04-16 16:44:16 -0400

    Display Passengers Screen and FXML Updates

--- src/application/DisplayPassengersController.java
+++ src/application/DisplayPassengersController.java
@@ -1,7 +1,20 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class DisplayPassengersController extends MainController
 {
+	@FXML
+	private TextField inputFlightNumber;
+	@FXML
+	private TextArea console;
+	
+	public void showFlightReservation()
+	{
+		console.setText(FlightSeating.getSeatingChart(inputFlightNumber.getText()));
+	}
+	
 	public void goToMainMenu()
 	{
 		changeScene("mainMenu");
